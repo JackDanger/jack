828bc25ac95e742ce2d283deaa34b2ddcecdd7a3
Jack Danger Canty <git@6brand.com>
2008-08-06 14:55:39 -0700

    crafting extremely simple test app.

---
+++ test/hello_world.js
@@ -0,0 +1,3 @@
+Jack.run(function(env){
+  return [200, {'Content-Type': 'text/html'}, '<h1>Hello World</h1>']
+})
--- test/spidermonkey_app.js
+++
@@ -1,3 +0,0 @@
-Jack.run(function(){
-  return [200, {'Content-Type': 'text/html'}, '<h1>SpiderMonkey</h1>']
-})
