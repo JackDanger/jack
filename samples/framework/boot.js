d871c732a1cfbe960961b422ab007f70c6964d0a
cohitre <carlosrr@gmail.com>
2009-02-02 10:11:31 -0800

    Adding more complicated View sample

--- samples/framework/boot.js
+++ samples/framework/boot.js
@@ -2,8 +2,25 @@ Jack.Template( "index" , "./samples/framework/templates/index.html.haml" );
 
 Jack.View( "indexView" , function( name ){
     window.document = Jack.Template("index");
-    $("body").append( name );
-    return document.innerHTML;
+    return $("body")
+            .find(".content")
+                .html( 
+                    baconl("%p This text was added dynamically by: ")
+                )
+                .append( name )
+            .end()
+            .append(
+                baconl("%ul.links")
+            )
+            .find(".links")
+                .append(
+                    "<li><a href='/user/1'>User 1</a></li>" , 
+                    "<li><a href='/user/2'>User 2</a></li>"
+                )
+             .end()
+        .end()
+        .html();
+
 });
 
 Jack.Action(/^\/$/,function() {
--- samples/hello.js
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env jack
-
-function(env){
-  return [200, {'Content-Type': 'text/html'}, '<h1>Hello World</h1>'];
-}
