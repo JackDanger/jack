2a2b584b066096089c35794fed37b1f3dbd614e5
Jack Danger Canty <gitcommit@6brand.com>
2009-03-10 21:19:53 -0700

    moving the Mongrel load to jack.js

--- lib/jack.rb
+++ lib/jack.rb
@@ -12,14 +12,13 @@ module Jack
     @js.load(File.dirname(__FILE__) + "/jack.js")
     Dir.glob(File.dirname(__FILE__) + "/jack/*.js").each { |f| @js.load(f) }
 
-    app = @js.load(app_file)
-
     @log = Logger.new(options.log)
     @log.info "Starting #{app_file} on http://#{options.host}:#{options.port}"
     @js['Jack']['root'] = File.dirname(app_file)
     @js['Jack']['log']  = @log # Why can't we go the other way? (Jack.log = Ruby.Jack.log)
+    @js['Jack']['mongrelOptions'] = {:Port => options.port, :Host => options.host}
 
-    Rack::Handler::Mongrel.run(app, {:Port => options.port, :Host => options.host})
+    @js.load(app_file)
   end
 
   def self.log; @log end
